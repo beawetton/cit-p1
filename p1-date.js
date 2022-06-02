e56b2eda9ef183f36be33af524c514635357aae7
beawetton <105769900+beawetton@users.noreply.github.com>
2022-06-02 01:16:01 -0700

    Create p1-date.js

---
+++ p1-date.js
@@ -0,0 +1,8 @@
+/*
+    CIT 281 Project 1
+    Name: Bea Wetton
+*/
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = new Date().getDay();
+console.log(days[day]);
